fff130fb85fec32a5698bdcb0c4965fbb0ef4f16
Psykopaten <104300989+Psykopaten@users.noreply.github.com>
2022-05-13 18:23:48 +0200

    Add files via upload

---
+++ psykopaten-ls/ReadMe.txt
@@ -0,0 +1,7 @@
+Hello!
+This loading screen is not made by me. I just remade it
+
+
+Orginal Link Here
+Kosta Loading Screen: https://forum.cfx.re/t/release-loadingscreen-animated-kosta-loading/1359465
+Made By: Kosta
---
+++ psykopaten-ls/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Psykopaten'
+version '1.0'
+description 'A Simple loading screen with video'
+
+loadscreen 'loading.html'
+client_script 'client/main.lua'
+
+files {
+    "css/font.css",
+    "css/plugins.css",
+    "css/style.css",
+    "js/music-controls.js",
+    "js/music-handler.js",
+    "mp4/LoadingScreen.mp4",
+    "webfonts/BAUHS93.eot",
+    "webfonts/BAUHS93.ttf",
+    "webfonts/BAUHS93.woff",
+    "webfonts/HARLOWSI_1.TTF",
+    "load.mp3",
+    "loading.html",
+    "logo.png"
+}
